0194205b7b0a77c5b997145036a45cfd90e11ac6
matthewdelorenzo <matthewdelorenzo@tamu.edu>
2022-11-28 00:05:29 -0600

    Removing venv from git

--- test.py
+++ test.py
@@ -19,3 +19,4 @@ with pd.option_context('display.max_rows', None, 'display.max_columns', None):  
 #    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
 #         print(df)
 #    print(dates_all)
+#test
